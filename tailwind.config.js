cc0b6ee6447ee20b239970976c8a4a4b4d6dd07e
alfarezyyd <adityaalfarezyd@gmail.com>
2024-06-08 22:23:53 +0700

    feat: styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
+// tailwind.config.js
+const {nextui} = require("@nextui-org/react");
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: () => ({
+        brand: {
+          50: "#E9E3FF",
+          100: "#C0B8FE",
+          200: "#A195FD",
+          300: "#8171FC",
+          400: "#7551FF",
+          500: "#422AFB",
+          600: "#3311DB",
+          700: "#2111A5",
+          800: "#190793",
+          900: "#11047A",
+        },
+      }),
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
